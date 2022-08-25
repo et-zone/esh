4325036693ecd5182869cb961e0d0bed9e64e168
Felix <2250219329@qq.com>
2022-08-25 16:23:13 +0800

    Update microK8sInstall.sh

--- k8s/microK8sInstall.sh
+++ k8s/microK8sInstall.sh
@@ -1,4 +1,6 @@
 snap remove microk8s
+curl -L "https://raw.githubusercontent.com/OpsDocker/pullk8s/main/pullk8s.sh" -o /usr/local/bin/pullk8s
+chmod +x /usr/local/bin/pullk8s
 snap install microk8s --classic --channel=1.21/stable
 snap alias microk8s.kubectl kubectl
 pullk8s pull k8s.gcr.io/pause:3.1 --microk8s
