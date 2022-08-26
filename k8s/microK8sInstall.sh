6904d520bec649917b0e4623ce62fee0c2921988
Felix <2250219329@qq.com>
2022-08-26 11:55:35 +0800

    Update microK8sInstall.sh

--- k8s/microK8sInstall.sh
+++ k8s/microK8sInstall.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 # install snap ,check cmd is install
-cmd1=`snap`
 
-if [[$cmd1==snap:*]]; then
+#if test -f /usr/bin/snap;then
+#echo "exit"
+#fi
+
+if test -f /usr/bin/snap; then
+echo "snap already installed "
+else
    #echo "not found"
    yum install epel-release
    yum install yum-plugin-copr
@@ -12,34 +17,41 @@ if [[$cmd1==snap:*]]; then
    systemctl enable --now snapd.socket
    ln -s /var/lib/snapd/snap /snap
    export PATH=$PATH:~/.jx/bin:/snap/bin
-else
-echo "snap already installed "
 fi
 
 # install timezone ,check cmd is install
-cmd2=`ntpdate`  
 
-if [[$cmd2==ntpdate:*]]; then
+if test -f /usr/sbin/ntpdate; then
+echo "ntpdate already installed "
+else
    #echo "not found"
    yum install ntpdate
    ntpdate ntp.sjtu.edu.cn
    timedatectl set-timezone Asia/Shanghai
-else
-echo "ntpdate already installed "
 fi
 
-cmd3=`pullk8s`
 
-if [[$cmd3==pullk8s:*]]; then
+if test -f /usr/local/bin/pullk8s; then
+echo "pullk8s already installed "
+else
    #echo "not found"
    curl -L "https://raw.githubusercontent.com/OpsDocker/pullk8s/main/pullk8s.sh" -o /usr/local/bin/pullk8s
    chmod +x /usr/local/bin/pullk8s
-else
-echo "pullk8s already installed "
 fi
 
+if grep "SNAPPATH=" ~/.bashrc;then
+echo ""
+else
+echo -e 'SNAPPATH=~/.jx/bin:/snap/bin \nexport PATH=$PATH:SNAPPATH' >> ~/.bashrc
+fi
+source ~/.bashrc
+
 # install k8s
+
+if test -f /snap/bin/microk8s;then
 snap remove microk8s
+fi
+
 snap install microk8s --classic --channel=1.21/stable
 snap alias microk8s.kubectl kubectl
 pullk8s pull k8s.gcr.io/pause:3.1 --microk8s
