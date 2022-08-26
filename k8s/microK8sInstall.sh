0a3cf74e0564c035e04f65c48f3bfd5e437f84ed
Felix <2250219329@qq.com>
2022-08-26 14:43:05 +0800

    Update microK8sInstall.sh

--- k8s/microK8sInstall.sh
+++ k8s/microK8sInstall.sh
@@ -31,14 +31,6 @@ else
 fi
 
 
-if test -f /usr/local/bin/pullk8s; then
-echo "pullk8s already installed "
-else
-   #echo "not found"
-   curl -L "https://raw.githubusercontent.com/OpsDocker/pullk8s/main/pullk8s.sh" -o /usr/local/bin/pullk8s
-   chmod +x /usr/local/bin/pullk8s
-fi
-
 if grep "SNAPPATH=" ~/.bashrc;then
 echo ""
 else
@@ -54,6 +46,16 @@ fi
 
 snap install microk8s --classic --channel=1.21/stable
 snap alias microk8s.kubectl kubectl
+
+if test -f /usr/local/bin/pullk8s; then
+echo "pullk8s already installed "
+else
+   #echo "not found"
+   curl -L "https://raw.githubusercontent.com/OpsDocker/pullk8s/main/pullk8s.sh" -o /usr/local/bin/pullk8s
+   chmod +x /usr/local/bin/pullk8s
+fi
+
+
 pullk8s pull k8s.gcr.io/pause:3.1 --microk8s
 pullk8s pull k8s.gcr.io/metrics-server-amd64:v0.3.6 --microk8s
 microk8s enable dashboard dns registry
